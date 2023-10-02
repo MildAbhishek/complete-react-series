727ac2201791721dae99b19b43da3cb2e09372b7
AfterAbhishek <abhishekanand8357@gmail.com>
2023-10-03 00:22:48 +0530

    custom-react-implementation

--- 01firstviteapp/src/main.jsx
+++ 01firstviteapp/src/main.jsx
@@ -3,6 +3,33 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const reactElement = {
+  type: 'a',
+  props: {
+    href: "https://google.com",
+    target: '_blank'
+
+  },
+  childen: 'Click me to visit google'
+}
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom React app</h1>
+    </div>
+  )
+}
+const AnotherElement = (
+  <a href="http://google.com" target='_blank'>Visit Google</a>
+)
+
+const areactElement = React.createElement(
+  'a',
+  { href: 'https://google.com', target: "_blank" },
+  'click to visit google',
+
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
